010195a7f66319bf21a8cf7ee29df0cd29be275f
Olga Kuznetsova <ollie.kuznetsova@gmail.com>
2024-07-10 06:07:36 +0200

    Updated

---
+++ scripts/4a. Creating pseudo-absences (20 samples).R
@@ -0,0 +1,162 @@
+## Sampling occurrences and creating pseudo-absences
+library(virtualspecies) # Generate virtual species distribution data for simulations
+library(geodata)       # Download and manage geographic data for analysis
+library(terra)         # Manipulate and analyze geographic data 
+library(spThin)        # Perform spatial thinning of the background data
+
+
+# Creating pseudo-absences (x10, x5, x3, x1) 
+
+## a. For 20 sampled presences ----
+
+### Pseudo-absences = presences x10 ----
+# Randomly select background data within the buffer, excluding presence locations.
+bg_rand_buf_20_10 <- terra::spatSample(region_buf_exclp20, length(presences20)*10, 
+                                       "random", na.rm=T, as.points=TRUE, 
+                                       exhaustive=T) # to ensure that we find enough samples
+
+plot(bg, col='grey90', legend=F)
+plot(region_buf20, add=T, col='grey60', legend=F)
+points(bg_rand_buf_20_10, pch=19, cex=0.2)
+points(presences20, pch=19, cex=0.5, col='red')
+
+
+## Joining presence and absence data
+# First, we prepare the presences data to contain a column indicating 1 for presence.
+sp_env_20_10 <- data.frame(sp_coords20, occ=1)
+# Rename columns
+colnames(sp_env_20_10)[colnames(sp_env_20_10) == "x"] <- "decimalLongitude"
+colnames(sp_env_20_10)[colnames(sp_env_20_10) == "y"] <- "decimalLatitude"
+
+# Second, we make sure the background data have the same columns, and indicate
+# 0 for absence.
+bg_rand_buf_df_20_10 <- data.frame(terra::geom(bg_rand_buf_20_10)[,c('x','y')])
+names(bg_rand_buf_df_20_10) <- c('decimalLongitude','decimalLatitude')
+bg_rand_buf_df_20_10$occ <- 0
+summary(bg_rand_buf_df_20_10)
+
+# Third, we bind these two data sets
+sp_env_20_10 <- rbind(sp_env_20_10, bg_rand_buf_df_20_10)
+summary(sp_env_20_10)
+
+# Last, we join this combined data set with the climate data.
+sp_env_20_10 <- cbind(sp_env_20_10, terra::extract(x = australia_clim1km, 
+                                                   y = sp_env_20_10[,c('decimalLongitude',
+                                                                       'decimalLatitude')], 
+                                                   cells=T) )
+summary(sp_env_20_10)
+
+
+
+### Pseudo-absences = presences x5 ----
+
+# Randomly select background data within the buffer, excluding presence locations.
+bg_rand_buf_20_5 <- terra::spatSample(region_buf_exclp20, length(presences20)*5, 
+                                       "random", na.rm=T, as.points=TRUE, 
+                                       exhaustive=T) # to ensure that we find enough samples
+
+plot(bg, col='grey90', legend=F)
+plot(region_buf20, add=T, col='grey60', legend=F)
+points(bg_rand_buf_20_5, pch=19, cex=0.2)
+points(presences20, pch=19, cex=0.5, col='red')
+
+
+## Joining presence and absence data
+# First, we prepare the presences data to contain a column indicating 1 for presence.
+sp_env_20_5 <- data.frame(sp_coords20, occ=1)
+# Rename columns
+colnames(sp_env_20_5)[colnames(sp_env_20_5) == "x"] <- "decimalLongitude"
+colnames(sp_env_20_5)[colnames(sp_env_20_5) == "y"] <- "decimalLatitude"
+
+# Second, we make sure the background data have the same columns, and indicate
+# 0 for absence.
+bg_rand_buf_df_20_5 <- data.frame(terra::geom(bg_rand_buf_20_5)[,c('x','y')])
+names(bg_rand_buf_df_20_5) <- c('decimalLongitude','decimalLatitude')
+bg_rand_buf_df_20_5$occ <- 0
+summary(bg_rand_buf_df_20_5)
+
+# Third, we bind these two data sets
+sp_env_20_5 <- rbind(sp_env_20_5, bg_rand_buf_df_20_5)
+summary(sp_env_20_5)
+
+# Last, we join this combined data set with the climate data.
+sp_env_20_5 <- cbind(sp_env_20_5, terra::extract(x = australia_clim1km, 
+                                                   y = sp_env_20_5[,c('decimalLongitude',
+                                                                       'decimalLatitude')], 
+                                                   cells=T) )
+summary(sp_env_20_5)
+### Pseudo-absences = presences x3 ----
+
+# Randomly select background data within the buffer, excluding presence locations.
+bg_rand_buf_20_3 <- terra::spatSample(region_buf_exclp20, length(presences20)*3, 
+                                      "random", na.rm=T, as.points=TRUE, 
+                                      exhaustive=T) # to ensure that we find enough samples
+
+plot(bg, col='grey90', legend=F)
+plot(region_buf20, add=T, col='grey60', legend=F)
+points(bg_rand_buf_20_3, pch=19, cex=0.2)
+points(presences20, pch=19, cex=0.5, col='red')
+
+
+## Joining presence and absence data
+# First, we prepare the presences data to contain a column indicating 1 for presence.
+sp_env_20_3 <- data.frame(sp_coords20, occ=1)
+# Rename columns
+colnames(sp_env_20_3)[colnames(sp_env_20_3) == "x"] <- "decimalLongitude"
+colnames(sp_env_20_3)[colnames(sp_env_20_3) == "y"] <- "decimalLatitude"
+
+# Second, we make sure the background data have the same columns, and indicate
+# 0 for absence.
+bg_rand_buf_df_20_3 <- data.frame(terra::geom(bg_rand_buf_20_3)[,c('x','y')])
+names(bg_rand_buf_df_20_3) <- c('decimalLongitude','decimalLatitude')
+bg_rand_buf_df_20_3$occ <- 0
+summary(bg_rand_buf_df_20_3)
+
+# Third, we bind these two data sets
+sp_env_20_3 <- rbind(sp_env_20_3, bg_rand_buf_df_20_3)
+summary(sp_env_20_3)
+
+# Last, we join this combined data set with the climate data.
+sp_env_20_3 <- cbind(sp_env_20_3, terra::extract(x = australia_clim1km, 
+                                                 y = sp_env_20_3[,c('decimalLongitude',
+                                                                    'decimalLatitude')], 
+                                                 cells=T) )
+summary(sp_env_20_3)
+
+### Pseudo-absences = presences x1 ----
+
+# Randomly select background data within the buffer, excluding presence locations.
+bg_rand_buf_20_1 <- terra::spatSample(region_buf_exclp20, length(presences20)*1, 
+                                      "random", na.rm=T, as.points=TRUE, 
+                                      exhaustive=T) # to ensure that we find enough samples
+
+plot(bg, col='grey90', legend=F)
+plot(region_buf20, add=T, col='grey60', legend=F)
+points(bg_rand_buf_20_1, pch=19, cex=0.2)
+points(presences20, pch=19, cex=0.5, col='red')
+
+
+## Joining presence and absence data
+# First, we prepare the presences data to contain a column indicating 1 for presence.
+sp_env_20_1 <- data.frame(sp_coords20, occ=1)
+# Rename columns
+colnames(sp_env_20_1)[colnames(sp_env_20_1) == "x"] <- "decimalLongitude"
+colnames(sp_env_20_1)[colnames(sp_env_20_1) == "y"] <- "decimalLatitude"
+
+# Second, we make sure the background data have the same columns, and indicate
+# 0 for absence.
+bg_rand_buf_df_20_1 <- data.frame(terra::geom(bg_rand_buf_20_1)[,c('x','y')])
+names(bg_rand_buf_df_20_1) <- c('decimalLongitude','decimalLatitude')
+bg_rand_buf_df_20_1$occ <- 0
+summary(bg_rand_buf_df_20_1)
+
+# Third, we bind these two data sets
+sp_env_20_1 <- rbind(sp_env_20_1, bg_rand_buf_df_20_1)
+summary(sp_env_20_1)
+
+# Last, we join this combined data set with the climate data.
+sp_env_20_1 <- cbind(sp_env_20_1, terra::extract(x = australia_clim1km, 
+                                                 y = sp_env_20_1[,c('decimalLongitude',
+                                                                    'decimalLatitude')], 
+                                                 cells=T) )
+summary(sp_env_20_1)
---
+++ scripts/5a. Spatial thinning (20 samples).R
@@ -0,0 +1,165 @@
+## Sampling occurrences and creating pseudo-absences
+library(virtualspecies) # Generate virtual species distribution data for simulations
+library(geodata)       # Download and manage geographic data for analysis
+library(terra)         # Manipulate and analyze geographic data 
+library(spThin)        # Perform spatial thinning of the background data
+
+#Make sure you download the virtual species you created and
+# upload the raster file of our region
+my.species <- readRDS("MyVirtualSpecies.RDS")
+australia_clim1km <- rast("data/australia_clim1km.tif") 
+
+
+# a. Spatial thinning for dfs of 20 presences 
+
+## Pseudo-absences = presences x10 ----
+
+# thin() expects that the data.frame contains a column with the species name
+sp_env_20_10$sp <- 'Virtual_species'
+
+# Remove adjacent cells of presence/background data:
+xy_20_10 <- thin(sp_env_20_10, lat.col='decimalLatitude',long.col='decimalLongitude',
+           spec.col='sp',
+           thin.par=30,
+           reps=1, 
+           write.files=F,
+           locs.thinned.list.return=T)
+
+# Keep the coordinates with the most presence records
+xy_keep_20_10 <- xy_20_10[[1]]
+
+# Thin the dataset - here, we first extract the cell numbers for the thinned 
+# coordinates and then use these to subset our data frame.
+cells_thinned_20_10 <- terra::cellFromXY(australia_clim1km, xy_keep_20_10)
+sp_thinned_20_10 <- sp_env_20_10[sp_env_20_10$cell %in% cells_thinned_20_10,]
+
+# Plot the map and data
+plot(bg, col='grey90', legend=F)
+points(sp_thinned_20_10[,1:2], pch=19,
+       col=c('black','red')[as.factor(sp_thinned_20_10$occ)], cex=0.3)
+
+save(sp_thinned_20_10, file='data/VS_Presx20Absx10_thinned.RData')
+
+
+
+## Pseudo-absences = presences x5 ----
+
+# thin() expects that the data.frame contains a column with the species name
+sp_env_20_5$sp <- 'Virtual_species'
+
+# Remove adjacent cells of presence/background data:
+xy_20_5 <- thin(sp_env_20_5, lat.col='decimalLatitude',long.col='decimalLongitude',
+                 spec.col='sp',
+                 thin.par=30,
+                 reps=1, 
+                 write.files=F,
+                 locs.thinned.list.return=T)
+
+# Keep the coordinates with the most presence records
+xy_keep_20_5 <- xy_20_5[[1]]
+
+# Thin the dataset - here, we first extract the cell numbers for the thinned 
+# coordinates and then use these to subset our data frame.
+cells_thinned_20_5 <- terra::cellFromXY(australia_clim1km, xy_keep_20_5)
+sp_thinned_20_5 <- sp_env_20_5[sp_env_20_5$cell %in% cells_thinned_20_5,]
+
+# Plot the map and data
+plot(bg, col='grey90', legend=F)
+points(sp_thinned_20_5[,1:2], pch=19,
+       col=c('black','red')[as.factor(sp_thinned_20_5$occ)], cex=0.3)
+
+save(sp_thinned_20_5, file='data/VS_Presx20Absx5_thinned.RData')
+
+
+
+
+
+## Pseudo-absences = presences x3 ----
+
+
+# thin() expects that the data.frame contains a column with the species name
+sp_env_20_3$sp <- 'Virtual_species'
+
+# Remove adjacent cells of presence/background data:
+xy_20_3 <- thin(sp_env_20_3, lat.col='decimalLatitude',long.col='decimalLongitude',
+                spec.col='sp',
+                thin.par=30,
+                reps=1, 
+                write.files=F,
+                locs.thinned.list.return=T)
+
+# Keep the coordinates with the most presence records
+xy_keep_20_3 <- xy_20_3[[1]]
+
+# Thin the dataset - here, we first extract the cell numbers for the thinned 
+# coordinates and then use these to subset our data frame.
+cells_thinned_20_3 <- terra::cellFromXY(australia_clim1km, xy_keep_20_3)
+sp_thinned_20_3 <- sp_env_20_3[sp_env_20_3$cell %in% cells_thinned_20_3,]
+
+# Plot the map and data
+plot(bg, col='grey90', legend=F)
+points(sp_thinned_20_3[,1:2], pch=19,
+       col=c('black','red')[as.factor(sp_thinned_20_3$occ)], cex=0.3)
+
+save(sp_thinned_20_3, file='data/VS_Presx20Absx3_thinned.RData')
+
+
+
+## Pseudo-absences = presences x1 ----
+
+
+# thin() expects that the data.frame contains a column with the species name
+sp_env_20_1$sp <- 'Virtual_species'
+
+# Remove adjacent cells of presence/background data:
+xy_20_1 <- thin(sp_env_20_1, lat.col='decimalLatitude',long.col='decimalLongitude',
+                spec.col='sp',
+                thin.par=30,
+                reps=1, 
+                write.files=F,
+                locs.thinned.list.return=T)
+
+# Keep the coordinates with the most presence records
+xy_keep_20_1 <- xy_20_1[[1]]
+
+# Thin the dataset - here, we first extract the cell numbers for the thinned 
+# coordinates and then use these to subset our data frame.
+cells_thinned_20_1 <- terra::cellFromXY(australia_clim1km, xy_keep_20_1)
+sp_thinned_20_1 <- sp_env_20_1[sp_env_20_1$cell %in% cells_thinned_20_1,]
+
+# Plot the map and data
+plot(bg, col='grey90', legend=F)
+points(sp_thinned_20_1[,1:2], pch=19,
+       col=c('black','red')[as.factor(sp_thinned_20_1$occ)], cex=0.3)
+
+save(sp_thinned_20_1, file='data/VS_Presx20Absx1_thinned.RData')
+
+
+
+## Plot the results
+
+# Set up a 2x2 plotting layout
+par(mfrow=c(2,2), oma=c(0, 0, 4, 0))  # Adjust outer margins to make space for the main title
+
+# Plot for x10 absences
+plot(bg, col='grey90', legend=FALSE, main="x10 absences")
+points(sp_thinned_20_10[,1:2], pch=19, col=c('black', 'red')[as.factor(sp_thinned_20_10$occ)], cex=0.3)
+
+# Plot for x5 absences
+plot(bg, col='grey90', legend=FALSE, main="x5 absences")
+points(sp_thinned_20_5[,1:2], pch=19, col=c('black', 'red')[as.factor(sp_thinned_20_5$occ)], cex=0.3)
+
+# Plot for x3 absences
+plot(bg, col='grey90', legend=FALSE, main="x3 absences")
+points(sp_thinned_20_3[,1:2], pch=19, col=c('black', 'red')[as.factor(sp_thinned_20_3$occ)], cex=0.3)
+
+# Plot for x1 absences
+plot(bg, col='grey90', legend=FALSE, main="x1 absences")
+points(sp_thinned_20_1[,1:2], pch=19, col=c('black', 'red')[as.factor(sp_thinned_20_1$occ)], cex=0.3)
+
+# Add the main title to the entire plotting area
+mtext("Thinned absences with thin(). Data with 20 presences.", outer=TRUE, line=2, cex=1.5)
+
+# Save the current plot to a PNG file
+dev.copy(png, filename="data/thinned_abs_plots_20prsncs.png", width=1600, height=1200, res=200)
+dev.off()  
